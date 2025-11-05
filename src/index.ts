bdf0ff718a2f8a6e734083e78ce3282371bd2def
Bryan Chu <bryanchu10@gmail.com>
2025-11-05 16:48:34 +0800

    新增 規則配置

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,26 @@ import { enforceFooBar } from "./rules/enforce-foo-bar";
 import { preferExplicitPropertyChecks } from "./rules/prefer-explicit-property-checks";
 import { preferLooseEquality } from "./rules/prefer-loose-equality";
 
-const plugin = {
-    rules: {
-        "enforce-foo-bar": enforceFooBar,
-        "prefer-explicit-property-checks": preferExplicitPropertyChecks,
-        "prefer-loose-equality": preferLooseEquality,
-    },
+export const rules = {
+    "enforce-foo-bar": enforceFooBar,
+    "prefer-explicit-property-checks": preferExplicitPropertyChecks,
+    "prefer-loose-equality": preferLooseEquality,
+};
+
+export const configs = {
+    recommended: {
+        plugins: ["relics"],
+        rules: {
+            "relics/enforce-foo-bar": "error",
+            "relics/prefer-explicit-property-checks": "error",
+            "relics/prefer-loose-equality": "error",
+        }
+    }
+}
+
+const plugin = { 
+    rules,
+    configs,
 };
 
 export default plugin;
-export const { rules } = plugin;
